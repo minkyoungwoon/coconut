1f47901fb2b690451b86fb2a5860f0ab6699c296
KyoungWoon Min <achieve-life@naver.com>
2022-10-24 21:56:24 +0900

    Initial commit

---
+++ src/main/java/com/project/coconut/CoconutApplication.java
@@ -0,0 +1,13 @@
+package com.project.coconut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoconutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoconutApplication.class, args);
+	}
+
+}
